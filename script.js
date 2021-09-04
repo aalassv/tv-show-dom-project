a609ca926fe46458d1f370380083126a14fd5a24
aalassv <a.alas.sv@gmail.com>
2021-09-04 18:09:42 +0100

    At index file adding the input field, at script.js I done the level 100

--- script.js
+++ script.js
@@ -4,9 +4,41 @@ function setup() {
   makePageForEpisodes(allEpisodes);
 }
 
+const rootElem = document.getElementById("root");
+
 function makePageForEpisodes(episodeList) {
-  const rootElem = document.getElementById("root");
-  rootElem.textContent = `Got ${episodeList.length} episode(s)`;
+  //rootElem.textContent = `Got ${episodeList.length} episode(s)`;
+  episodeList.forEach(createdFromList);
 }
 
+
+function createdFromList(episode) {
+  //console.log(episode.name, episode.image.medium);
+  let episodeDiv = document.createElement("div");
+  rootElem.appendChild(episodeDiv);
+
+  let episodeName = document.createElement("h2");
+  episodeDiv.appendChild(episodeName);
+  episodeName.innerText = episode.name;
+
+  let episodeSeason = document.createElement("p");
+  episodeDiv.appendChild(episodeSeason);
+  episodeSeason.innerText = `S0${episode.season}E0${episode.number}`;
+
+  let episodeImage = document.createElement("img");
+  episodeDiv.appendChild(episodeImage);
+  episodeImage.src = episode.image.medium;
+
+  let episodeSummary = document.createElement("span");
+  episodeDiv.appendChild(episodeSummary);
+  episodeSummary.innerHTML = episode.summary;
+
+}
+
+function searchElement () {
+  let episodeInfo = getAllEpisodes();
+  
+}
+
+  
 window.onload = setup;
