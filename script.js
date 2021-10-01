6659d21ad246438a458e5f94576f38850133ce68
aalassv <a.alas.sv@gmail.com>
2021-10-01 01:15:01 +0100

    level 400 thanks to Abadi.

--- script.js
+++ script.js
@@ -1,83 +1,155 @@
 //You can edit ALL of the code here
 // global variables
 const rootElem = document.getElementById("root");
-let inputValue = "";
-const allEpisodes = getAllEpisodes();
-let filterEpisodes; //search filter
-let inputSearch = document.querySelector("#search");
 let inputSelectDropDown = document.querySelector("#select__search");
-let buttonRefresh = document.querySelector("#reset__episodes");
-// main function to show web content.
+
 function setup() {
-  makePageForEpisodes(allEpisodes); // load all episodes
-  inputSearch.addEventListener("keyup", readInput);
-  loadSelectElement(allEpisodes); // fill select options
-  inputSelectDropDown.addEventListener("change", atSelectionSearch);
-  buttonRefresh.addEventListener("click", refreshEpisodes);
+  //makePageForEpisodes(allEpisodes); // load all episodes
+  fetch("https://api.tvmaze.com/shows/1632/episodes")
+    .then((response) => response.json())
+    .then((episodeList) => {
+      makePageForEpisodes(episodeList);
+      keyUpEventListener(episodeList);
+      clickRefreshPage(episodeList);
+      dropDownEventListener(episodeList);
+      loadSelectElement(episodeList);
+      //refreshEpisodes(episodeList);
+    });
 }
 
-function atSelectionSearch(episode) {
-  let episodeSelected = inputSelectDropDown.value;
-  optionSelected = allEpisodes.filter(
-    //**I need to redo the like an outside function
-    (episode) =>
-      episode.name
-        .toLowerCase()
-        .includes(episodeSelected.substring(10).toLowerCase()) ||
-      episode.summary
-        .toLowerCase()
-        .includes(episodeSelected.substring(10).toLowerCase())
-  );
-  rootElem.innerHTML = "";
-  makePageForEpisodes(optionSelected);
-
-  let displaying = document.querySelector("#displayCounter");
-  displaying.innerText = `Displaying ${optionSelected.length} / ${allEpisodes.length} Episodes`;
-  displaying.style.color = "red";
-}
-
-function refreshEpisodes() {
-  rootElem.innerHTML = "";
-  makePageForEpisodes(allEpisodes);
-
-  let displaying = document.querySelector("#displayCounter");
-  displaying.innerText = `Displaying ${allEpisodes.length} / ${allEpisodes.length} Episodes`;
-  displaying.style.color = "red";
-}
-// function to filter input search and display the amount of episodes founded.
-function readInput(event) {
-  inputValue = event.target.value;
-  filterEpisodes = allEpisodes.filter(
-    //**I need to redo the like an outside function
-    (episode) =>
-      episode.name.toLowerCase().includes(inputValue.toLowerCase()) ||
-      episode.summary.toLowerCase().includes(inputValue.toLowerCase())
-  );
-  //cleaning the rootElem to update every time
-  rootElem.innerHTML = "";
-  makePageForEpisodes(filterEpisodes);
-  //rendering the amount of episodes founded.
-  let displaying = document.querySelector("#displayCounter");
-  displaying.innerText = `Displaying ${filterEpisodes.length} / ${allEpisodes.length} Episodes`;
-  displaying.style.color = "red";
-}
-
-//load all episodes on the select element and let pick one and show it.
-function loadSelectElement(event) {
-  //p.innerText = `You have selected: ${p.style.color}`; //+ value;
+//select a different Show
+function showSelector(show) {
+  let select = document.getElementById("show__selector");
   let option = document.createElement("option");
-  inputSelectDropDown.appendChild(option);
-  option.innerText = "Select episode";
+  option.value = show.name;
+  option.innerText = show.name;
+  select.appendChild(option);
+}
+
+function showDropDownList(episodes) {
+  episodes.forEach(showSelector);
+  eventListenerShowDropDown(episodes);
+}
+
+let allShows = getAllShows();
+showDropDownList(allShows);
+
+function eventListenerShowDropDown(shows) {
+  let selected = document.getElementById("show__selector");
+  selected.addEventListener("change", function () {
+    let optionElement = document.querySelectorAll("option");
+    let names = [];
+    optionElement.forEach((option) => names.push(option.value));
+    let selectedName = names.filter((name) => selected.value === name);
+    let getOneShow = shows.filter((show) => show.name == selectedName);
+    let showId = getOneShow[0].id;
+    console.log(showId);
+    async function catchId(id) {
+      fetch(`https://api.tvmaze.com/shows/${id}/episodes`)
+        .then((response) => response.json())
+        .then((episodeList) => {
+          makePageForEpisodes(episodeList);
+          keyUpEventListener(episodeList);
+          clickRefreshPage(episodeList);
+          dropDownEventListener(episodeList);
+          loadSelectElement(episodeList);
+        });
+    }
+    catchId(showId);
+  });
+}
+
+//Dropdown list for episodes
+function dropDownEventListener(episodes) {
+  let selectId = document.getElementById("select__search");
+  selectId.addEventListener("change", function () {
+    let optionsElements = document.querySelectorAll("option");
+    let names = [];
+    optionsElements.forEach((option) => names.push(option.value));
+    let selectedName = names.filter((name) => selectId.value === name);
+    let oneEpisode = episodes.filter((episode) => episode.name == selectedName);
+    console.log(oneEpisode);
+    if (oneEpisode.length === 1) {
+      makePageForEpisodes(oneEpisode);
+      console.log("hello");
+    } else {
+      makePageForEpisodes(episodes);
+    }
+  });
+
+  /*inputSelectDropDown.addEventListener("change", function () {
+    let episodeSelected = inputSelectDropDown.value;
+    //console.log(episodeSelected);
+    optionSelected = episodes.filter((episode) => {
+      episode.name.includes(episodeSelected);
+      console.log("Hello");
+    });
+    //rootElem.innerHTML = "";
+    makePageForEpisodes(optionSelected);
+
+    let displaying = document.querySelector("#displayCounter");
+    displaying.innerText = `Displaying ${optionSelected.length} / ${episodes.length} Episodes`;
+    displaying.style.color = "red";
+  });  */
+}
+
+//Refresh page
+function clickRefreshPage(episodes) {
+  let buttonRefresh = document.querySelector("#reset__episodes");
+  buttonRefresh.addEventListener("click", refreshEpisodes);
+  function refreshEpisodes() {
+    rootElem.innerHTML = "";
+    makePageForEpisodes(episodes);
+
+    let displaying = document.querySelector("#displayCounter");
+    displaying.innerText = `Displaying ${episodes.length} / ${episodes.length} Episodes`;
+    displaying.style.color = "red";
+  }
+}
+
+//Input-Search
+function keyUpEventListener(episodes) {
+  let inputSearch = document.querySelector("#search");
+  inputSearch.addEventListener("keyup", readInput);
+  function readInput(event) {
+    let inputValue = event.target.value;
+    let filterEpisodes = episodes.filter(
+      (episode) =>
+        episode.name.toLowerCase().includes(inputValue.toLowerCase()) ||
+        episode.summary.toLowerCase().includes(inputValue.toLowerCase())
+    );
+    rootElem.innerHTML = "";
+    makePageForEpisodes(filterEpisodes);
+    //console.log("Hello Bunny ears");
+    let displaying = document.querySelector("#displayCounter");
+    displaying.innerText = `Displaying ${filterEpisodes.length} / ${filterEpisodes.length} Episodes`;
+    displaying.style.color = "red";
+  }
+}
+
+function loadSelectElement(episodes) {
+  let refresh = document.getElementById("select__search");
+  refresh.replaceChildren([]);
+  let createOption = document.createElement("option");
+  createOption.value = "non-option";
+  createOption.innerText = "All episodes";
+  refresh.appendChild(createOption);
+  //p.innerText = `You have selected: ${p.style.color}`; //+ value;
+  // let option = document.createElement("option");
+  // inputSelectDropDown.appendChild(option);
+  // option.innerText = "Select episode";
   //console.log(event);
-  event.forEach((episode) => {
+  episodes.forEach((episode) => {
     let option1 = document.createElement("option");
     inputSelectDropDown.appendChild(option1);
     option1.innerText = `${zeroPadding(episode)} - ${episode.name}`;
+    option1.value = episode.name;
   });
 }
 
 // function to take the specific array and pass it true a for each to create every card.
 function makePageForEpisodes(currentValue) {
+  rootElem.replaceChildren([]);
   currentValue.forEach(individualEpisodes);
 }
 // function to create every element inside the rootElem.
@@ -89,9 +161,22 @@ function individualEpisodes(episode) {
   episodeDiv.appendChild(episodeName);
   episodeName.innerText = episode.name;
 
+  let seasonNum = episode.season.toString();
+  if (seasonNum.length === 1) {
+    seasonNum = "0" + seasonNum;
+  }
+
+  let episodeNum = episode.number.toString();
+  if (episodeNum.length === 1) {
+    episodeNum = "0" + episodeNum;
+  }
+  //newOption.innerText =
+  // "S" + seasonNum + "E" + episodeNum + " - " + episode.name;
+
   let episodeSeason = document.createElement("p");
   episodeDiv.appendChild(episodeSeason);
-  episodeSeason.innerText = zeroPadding(episode);
+  episodeSeason.innerText =
+    "S" + seasonNum + "E" + episodeNum + " - " + episode.name;
 
   let episodeImage = document.createElement("img");
   episodeDiv.appendChild(episodeImage);
@@ -103,6 +188,7 @@ function individualEpisodes(episode) {
 }
 //pull out the 0 if it is needed.
 function zeroPadding(episode) {
+  //episode = episodeListFetch;
   if (episode.number < 10) {
     return `S0${episode.season}E0${episode.number}`;
   } else {
